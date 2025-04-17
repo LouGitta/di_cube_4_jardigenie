30a5d97b0c01928423451ba15680607adf4817f9
Hyoy <ruhtra.liopmob@gmail.com>
2025-04-17 13:26:52 +0200

    ajout authcontroller fonctionnel (normalement)

--- front_office/index.php
+++ front_office/index.php
@@ -23,10 +23,72 @@ if(array_key_exists('route', $_GET)):
             $controller->displayByCat();
             break;
 
+        case 'addProduct':
+            if(!array_key_exists('ref', $_GET) || $_GET['ref'] != "add") {
+                // Afficher le formulaire
+                $controller = new Controllers\ProductController();
+                $controller->displayForm();
+            }else {
+                // Soumettre le formulaire
+                $controller = new Controllers\ProductController();
+                $controller->submitForm();
+            }
+            break;
+
+        case 'editProduct':
+            if(!array_key_exists('ref', $_GET) || $_GET['ref'] != "editProduct") {
+                // Afficher le formulaire
+                $controller = new Controllers\ProductController();
+                $controller->displayFormEditProduct($_GET['id']);
+            }else {
+                // Soumettre le formulaire
+                $controller = new Controllers\ProductController();
+                $controller->submitFormEditProduct();
+            }
+            break;
+
+        case 'deleteProduct':
+            if(isset($_GET['id']) && $_GET['id'] > 0) {
+                $controller = new Controllers\ProductController();
+                $controller->deleteProduct($_GET['id']);
+            }
+            header('location: index.php?route=home');
+            exit;
+            break; 
         
-        case 'nouscontacter' :
-            $controller = new Controllers\PageController();
-            $controller->displayContact();
+        case 'nouscontacter':
+          //  $controller = new Controllers\PageController();
+            // $controller->displayContact();
+            // break;
+            
+        // Routes pour l'authentification
+        case 'login':
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                // Traitement de la connexion
+                $controller = new Controllers\AuthController();
+                $controller->login();
+            } else {
+                // Affichage du formulaire de connexion
+                $controller = new Controllers\AuthController();
+                $controller->showLoginForm();
+            }
+            break;
+            
+        case 'register':
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                // Traitement de l'inscription
+                $controller = new Controllers\AuthController();
+                $controller->register();
+            } else {
+                // Affichage du formulaire d'inscription
+                $controller = new Controllers\AuthController();
+                $controller->showRegisterForm();
+            }
+            break;
+            
+        case 'logout':
+            $controller = new Controllers\AuthController();
+            $controller->logout();
             break;
             
         default:
