854d8a66a6ef625d444fbc16158abab9e04aceec
AboubacarDby <diabyaboubacar940@outlook.fr>
2025-04-18 11:13:45 +0200

    Modif config

--- front_office/config/config.php
+++ front_office/config/config.php
@@ -7,7 +7,7 @@ Constantes pour définir les paramètres de connexion à la base de données
 define("DB_HOST", 'localhost');
 define("DB_NAME", 'jardigenie');
 define("DB_USER", 'root');
-define("DB_PASS", 'Guinee1958.');
+define("DB_PASS", '');
 
 
 
